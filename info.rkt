022899e8c58f36550102995bd93511865e072640
Yanying Wang <yanyingwang1@gmail.com>
2020-08-16 18:22:28 +0800

    at-exp-lib

--- info.rkt
+++ info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection "http-client")
-(define deps '("base" "html-parsing"))
+(define deps '("base" "html-parsing" "at-exp-lib"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/http-client.scrbl" ())))
 (define pkg-desc "A practical http client library for sending data to http servers.")
