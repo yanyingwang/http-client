9ec7545c9f2799fd47957d6dc7c99982a23f400f
Yanying Wang <yanyingwang1@gmail.com>
2021-05-24 17:36:11 +0800

    refix github link

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang info
 (define collection "http-client")
 (define deps '("base" "html-parsing" "at-exp-lib"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib" "https://github.com/yanyingwang/scribble-rainbow-delimiters"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib" "https://github.com/yanyingwang/scribble-rainbow-delimiters.git"))
 (define scribblings '(("scribblings/http-client.scrbl" ())))
 (define pkg-desc "A practical http client library for sending data to http servers.")
 (define version "0.2")
